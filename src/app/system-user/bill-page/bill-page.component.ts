4458d47ebc12dc5237e825b7f94decf30135a63a
Artem <artyomartemyev@mail.ru>
2018-03-21 22:44:12 +0300

    featured umbrella-ui-service 6

--- src/app/system-user/bill-page/bill-page.component.ts
+++ src/app/system-user/bill-page/bill-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'wfm-bill-page',
@@ -10,6 +11,46 @@ export class BillPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready( () => {
+      $('.minus-button').click( (e) => {
+
+        // change this to whatever minimum you'd like
+        const minValue = 0;
+
+        const currentInput = $(e.currentTarget).parent().prev()[0];
+
+        let minusInputValue = $(currentInput).html();
+
+        if (minusInputValue > minValue) {
+          minusInputValue --;
+          $($(e.currentTarget).next()).removeAttr('disabled');
+          $(currentInput).html(minusInputValue);
+
+          if (minusInputValue <= minValue) {
+            $(e.currentTarget).attr('disabled', 'disabled');
+          }
+        }
+      });
+
+      $('.plus-button').click( (e) => {
+
+        const maxValue = 10;
+
+        const currentInput = $(e.currentTarget).parent().prev()[0];
+
+        let plusInputValue = $(currentInput).html();
+
+        if (plusInputValue < maxValue) {
+          plusInputValue ++;
+          $($(e.currentTarget).prev()[0]).removeAttr('disabled');
+          $(currentInput).html(plusInputValue);
+
+          if (plusInputValue >= maxValue) {
+            $(e.currentTarget).attr('disabled', 'disabled');
+          }
+        }
+      });
+    });
   }
 
 }
